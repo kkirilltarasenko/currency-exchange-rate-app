643e2822bf8505f2315d6c439623c920fedd6a6d
Kirill <k.tarasenko@atomgroup.io>
2026-05-31 16:27:19 +0300

    fix: update next config to generate out dir

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin('./i18n.ts');
+const withNextIntl = createNextIntlPlugin("./i18n.ts");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/home',
+        source: "/",
+        destination: "/home",
         permanent: true,
       },
-    ]
+    ];
   },
 };
 
