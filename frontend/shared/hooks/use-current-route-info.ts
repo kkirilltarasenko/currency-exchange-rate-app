bffba831e79c87bc3982b18fc9a3ea0751c858b0
Kirill <k.tarasenko@atomgroup.io>
2026-05-31 17:31:52 +0300

    fix: page title calculation

--- frontend/shared/hooks/use-current-route-info.ts
+++ frontend/shared/hooks/use-current-route-info.ts
@@ -2,18 +2,17 @@ import { usePathname } from "next/navigation";
 import { LINKS, type TRoute } from "../router/router.types";
 
 export const useCurrentRouteInfo = () => {
-  const pathnameArray = usePathname().split("/");
-  const pathname = pathnameArray[pathnameArray.length - 1];
+  const pathname = usePathname().replace(/\/$/, "");
 
   const getIsActiveRoute = (route?: TRoute) => {
     if (!route) {
       return false;
     }
-    return pathname === route;
+    return pathname.endsWith(route);
   };
 
   const getPageTitle = (): (typeof LINKS)[number]["titleKey"] | "404" => {
-    const pageTitle = LINKS.find(({ path }) => path === pathname);
+    const pageTitle = LINKS.find(({ path }) => pathname.endsWith(path));
 
     if (pageTitle) {
       return pageTitle.titleKey;
